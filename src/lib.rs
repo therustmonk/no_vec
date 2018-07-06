a7aad1b7d9dfa79405c4b9e3df94637bf59c75cd
Denis Kolodin <deniskolodin@gmail.com>
2018-07-06 21:22:34 +0300

    Add Concrete trait

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,10 @@ pub trait Unstick<T> {
     fn unstick(self) -> (Self::Target, T);
 }
 
+pub trait Concrete<T>: Sized {
+    fn concrete(self) -> Result<T, Self>;
+}
+
 macro_rules! impl_stick_unstick {
     ($from:expr, $to:expr) => {
         impl<T> Stick<T> for [T; $from] {
@@ -20,13 +24,12 @@ macro_rules! impl_stick_unstick {
             fn stick(self, item: T) -> Self::Target {
                 unsafe {
                     let mut result: Self::Target = mem::uninitialized();
-                    let len_and_last = self.len();
                     ptr::copy(
                         self.as_ptr(),
                         result.as_mut_ptr(),
-                        len_and_last,
+                        $from,
                     );
-                    result[len_and_last] = item;
+                    result[$from] = item;
                     result
                 }
             }
@@ -38,15 +41,29 @@ macro_rules! impl_stick_unstick {
             fn unstick(mut self) -> (Self::Target, T) {
                 unsafe {
                     let mut result: Self::Target = mem::uninitialized();
-                    let len_and_last = result.len();
                     ptr::copy(
                         self.as_ptr(),
                         result.as_mut_ptr(),
-                        len_and_last,
+                        $from,
                     );
                     let mut item: T = mem::uninitialized();
-                    ptr::swap(&mut item, &mut self[len_and_last]);
+                    ptr::swap(&mut item, &mut self[$from]);
                     (result, item)
+                }
+            }
+        }
+
+        impl<T> Concrete<[T; $from]> for Vec<T> {
+            fn concrete(self) -> Result<[T; $from], Self> {
+                if self.len() == $from {
+                    unsafe {
+                        let mut result: [T; $from] = mem::uninitialized();
+                        ptr::copy(self.as_ptr(), result.as_mut_ptr(), $from);
+                        drop(self);
+                        Ok(result)
+                    }
+                } else {
+                    Err(self)
                 }
             }
         }
@@ -71,20 +88,25 @@ impl_stick_unstick_all!(
 
 #[cfg(test)]
 mod tests {
-    use {Stick, Unstick};
+    use {Concrete, Stick, Unstick};
 
     #[test]
     fn test_stick_and_unstick() {
-        let arr = [123].stick(321);
+        let arr = [123u16].stick(321);
         assert_eq!(arr, [123, 321]);
 
         let arr = arr.stick(999);
         assert_eq!(arr, [123, 321, 999]);
 
-        let pair = arr.unstick();
-        assert_eq!(pair, ([123, 321], 999));
+        let vec = Box::new(arr).to_vec();
+        let arr: [u16; 3] = vec.concrete().unwrap();
 
-        let pair = pair.0.unstick();
-        assert_eq!(pair, ([123], 321));
+        let (arr, item) = arr.unstick();
+        assert_eq!(arr, [123, 321]);
+        assert_eq!(item, 999);
+
+        let (arr, item) = arr.unstick();
+        assert_eq!(arr, [123]);
+        assert_eq!(item, 321);
     }
 }

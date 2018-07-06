234b2ca618b9bb702f9b5ce5eb80e3f9b969c481
Denis Kolodin <deniskolodin@gmail.com>
2018-07-06 18:13:43 +0300

    Use macro to declare Stick and Unstick

--- src/lib.rs
+++ src/lib.rs
@@ -6,48 +6,54 @@ pub trait Stick<T> {
     fn stick(self, item: T) -> Self::Target;
 }
 
-impl<T> Stick<T> for [T; 1] {
-    type Target = [T; 2];
-
-    fn stick(self, item: T) -> Self::Target {
-        unsafe {
-            let mut result: Self::Target = mem::uninitialized();
-            let len_and_last = self.len();
-            ptr::copy(
-                self.as_ptr(),
-                result.as_mut_ptr(),
-                len_and_last,
-            );
-            result[len_and_last] = item;
-            result
-        }
-    }
-}
-
 pub trait Unstick<T> {
     type Target;
 
     fn unstick(self) -> (Self::Target, T);
 }
 
-impl<T> Unstick<T> for [T; 2] {
-    type Target = [T; 1];
+macro_rules! impl_stick_unstick {
+    [$from:expr ; $to:expr] => {
+        impl<T> Stick<T> for [T; $from] {
+            type Target = [T; $to];
 
-    fn unstick(mut self) -> (Self::Target, T) {
-        unsafe {
-            let mut result: Self::Target = mem::uninitialized();
-            let len_and_last = result.len();
-            ptr::copy(
-                self.as_ptr(),
-                result.as_mut_ptr(),
-                len_and_last,
-            );
-            let mut item: T = mem::uninitialized();
-            ptr::swap(&mut item, &mut self[len_and_last]);
-            (result, item)
+            fn stick(self, item: T) -> Self::Target {
+                unsafe {
+                    let mut result: Self::Target = mem::uninitialized();
+                    let len_and_last = self.len();
+                    ptr::copy(
+                        self.as_ptr(),
+                        result.as_mut_ptr(),
+                        len_and_last,
+                    );
+                    result[len_and_last] = item;
+                    result
+                }
+            }
         }
-    }
+
+        impl<T> Unstick<T> for [T; $to] {
+            type Target = [T; $from];
+
+            fn unstick(mut self) -> (Self::Target, T) {
+                unsafe {
+                    let mut result: Self::Target = mem::uninitialized();
+                    let len_and_last = result.len();
+                    ptr::copy(
+                        self.as_ptr(),
+                        result.as_mut_ptr(),
+                        len_and_last,
+                    );
+                    let mut item: T = mem::uninitialized();
+                    ptr::swap(&mut item, &mut self[len_and_last]);
+                    (result, item)
+                }
+            }
+        }
+    };
 }
+
+impl_stick_unstick![1;2];
 
 #[cfg(test)]
 mod tests {

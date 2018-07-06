c1dd520ff2ea02ff01ff215a574f301a6e551f96
Denis Kolodin <deniskolodin@gmail.com>
2018-07-06 18:07:39 +0300

    Add Unstick trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,63 @@
 use std::{mem, ptr};
 
-pub trait Appender {
-    type Item;
+pub trait Stick<T> {
     type Target;
 
-    fn append(self, item: Self::Item) -> Self::Target;
+    fn stick(self, item: T) -> Self::Target;
 }
 
-impl<T> Appender for [T; 1] {
-    type Item = T;
+impl<T> Stick<T> for [T; 1] {
     type Target = [T; 2];
 
-    fn append(self, item: Self::Item) -> Self::Target {
+    fn stick(self, item: T) -> Self::Target {
         unsafe {
             let mut result: Self::Target = mem::uninitialized();
+            let len_and_last = self.len();
             ptr::copy(
-                self.as_ptr() as *const u8,
-                result.as_mut_ptr() as *mut u8,
-                mem::size_of::<Self>()
+                self.as_ptr(),
+                result.as_mut_ptr(),
+                len_and_last,
             );
-            result[self.len()] = item;
+            result[len_and_last] = item;
             result
+        }
+    }
+}
+
+pub trait Unstick<T> {
+    type Target;
+
+    fn unstick(self) -> (Self::Target, T);
+}
+
+impl<T> Unstick<T> for [T; 2] {
+    type Target = [T; 1];
+
+    fn unstick(mut self) -> (Self::Target, T) {
+        unsafe {
+            let mut result: Self::Target = mem::uninitialized();
+            let len_and_last = result.len();
+            ptr::copy(
+                self.as_ptr(),
+                result.as_mut_ptr(),
+                len_and_last,
+            );
+            let mut item: T = mem::uninitialized();
+            ptr::swap(&mut item, &mut self[len_and_last]);
+            (result, item)
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use Appender;
+    use {Stick, Unstick};
 
     #[test]
     fn it_works() {
-        let arr: [u8; 2] = [123].append(321);
+        let arr: [u8; 2] = [123].stick(321);
         assert_eq!(arr, [123, 321]);
+        let pair = arr.unstick();
+        assert_eq!(pair, ([123], 321));
     }
 }
